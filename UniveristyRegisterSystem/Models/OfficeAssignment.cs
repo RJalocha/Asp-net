68302b01fa2d42dfa37663bd807e003a5a7fce80
Robert Jałocha <robert.jalocha@gmail.com>
2020-01-29 14:51:05 +0000

    Add Models

---
+++ UniveristyRegisterSystem/Models/OfficeAssignment.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using UniveristyRegisterSystem.Models;
+
+namespace UniveristyRegisterSystem.Models
+{
+    public class OfficeAssignment
+    {
+        [Key]
+        [ForeignKey("Teacher")]
+        public int InstructorID { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Miejsce pracy")]
+        public string Location { get; set; }
+
+        public virtual Teacher Teacher { get; set; }
+    }
+}
---
+++ UniveristyRegisterSystem/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace UniveristyRegisterSystem.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
